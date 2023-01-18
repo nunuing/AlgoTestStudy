b1317990cc21c311304a16c1eede0c7ec140b603
eycho <chw3872@gmail.com>
2023-01-18 21:38:30 +0900

    백준 11057 오르막 수

---
+++ src/Q11057.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q11057 {
+    public static final int DIV = 10007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
