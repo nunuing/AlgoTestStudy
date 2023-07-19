86c3ea8443c016451c426d54df5dcc8f68144e7d
eycho <chw3872@gmail.com>
2023-07-19 18:18:41 +0900

    Q16929 solving

--- src/Q16929.java
+++ src/Q16929.java
@@ -7,6 +7,8 @@ public class Q16929 {
     static char[][] board;
     static boolean[][] visited;
     static int n, m;
+    static Pair first;
+    static boolean check;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,31 +21,43 @@ public class Q16929 {
         for (int i = 0; i < n; i++) {
             board[i] = br.readLine().toCharArray();
         }
+        br.close();
 
+        check = false;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+                first = new Pair(i, j);
                 visited = new boolean[n][m];
                 visited[i][j] = true;
                 dfs(new Pair(i, j), 1);
-            }
-        }
-        System.out.println("NO");
-    }
-
-    static void dfs(Pair now, int level) {
-        int[] mx = {0, 0, 1, -1};
-        int[] my = {1, -1, 0, 1};
-
-        for (int i = 0; i < mx.length; i++){
-            int dx = now.x + mx[i];
-            int dy = now.y = my[i];
-            if((dx >= 0 && dx < n) && (dy >= 0 && dy < m) && (board[now.x][now.y] == board[dx][dy])){
-                if(!visited[dx][dy]){
-                    visited[dx][dy] = true;
+                if(check){
+                    System.out.println("Yes");
+                    return;
                 }
             }
         }
+        System.out.println("No");
+    }
 
+    static void dfs(Pair now, int level) {
+        int[] mx = {1, 0, -1, 0};
+        int[] my = {0, 1, 0, -1};
+
+        for (int i = 0; i < mx.length; i++) {
+            int dx = now.x + mx[i];
+            int dy = now.y + my[i];
+            if ((dx >= 0 && dx < n) && (dy >= 0 && dy < m) && (board[first.x][first.y] == board[dx][dy])) {
+                if (!visited[dx][dy]) {
+                    visited[dx][dy] = true;
+                    dfs(new Pair(dx, dy), level + 1);
+                } else {
+                    if (level >= 4 && (first.x == now.x) && (first.y == now.y)) {
+                        check = true;
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     static class Pair {
