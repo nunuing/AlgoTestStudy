5d0513549a28ab49eddfec9c0629ebc345986010
eycho <chw3872@gmail.com>
2023-07-26 23:24:38 +0900

    Q14226 solving

---
+++ src/Q14226.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q14226 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+
+        int screen = 1;
+        int clipboard = 0;
+
+        int cnt = 0;
+        while (screen != s){
+
+        }
+    }
+}
