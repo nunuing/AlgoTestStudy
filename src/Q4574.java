c83be1fbdbf05267a750aed7b5f01cdc84e13808
eycho <chw3872@gmail.com>
2023-08-12 22:00:13 +0900

    Q4574 solving

--- src/Q4574.java
+++ src/Q4574.java
@@ -6,6 +6,7 @@ public class Q4574 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
         br.close();
     }
 }
