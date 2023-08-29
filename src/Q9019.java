6817d681b6b36da7fd67a5a6d100ab33ac2f8ade
eycho <chw3872@gmail.com>
2023-08-29 22:39:06 +0900

    Q9019 solving

--- src/Q9019.java
+++ src/Q9019.java
@@ -1,2 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Q9019 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            Queue<Integer> queue = new LinkedList<>();
+            while (!queue.isEmpty()) {
+
+            }
+        }
+
+    }
 }
