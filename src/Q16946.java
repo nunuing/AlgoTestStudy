0d77d9265cef309018c037a290741e2b9ff92150
eycho <chw3872@gmail.com>
2023-09-05 20:48:05 +0900

    Q16946 solving

---
+++ src/Q16946.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q16946 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[n][m];
+        int[][] answer = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            char[] input = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = input[j] == 1 ? 1 : 0;
+            }
+        }
+
+    }
+}
