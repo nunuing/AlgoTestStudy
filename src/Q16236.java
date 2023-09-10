c78125dc6107c86dfe02ef983aa4880fea518c2a
eycho <chw3872@gmail.com>
2023-09-10 22:30:53 +0900

    Q16236

--- src/Q16236.java
+++ src/Q16236.java
@@ -1,93 +1,87 @@
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Q16236 {
-    static int n, size;
-    static int[][] sea;
     static Pair shark;
+    static int[] mx = {-1, 1, 0, 0};
+    static int[] my = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        sea = new int[n][n];
+        int[][] sea = new int[n][n];
         StringTokenizer st;
-        size = 1;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 sea[i][j] = Integer.parseInt(st.nextToken());
                 if (sea[i][j] == 9) {
-                   shark = new Pair(i, j, 0);
+                    sea[i][j] = 0;
+                    shark = new Pair(i, j, 0);
                 }
             }
         }
-        PriorityQueue<Pair> fishes = new PriorityQueue<>(new Comparator<Pair>() {
-            @Override
-            public int compare(Pair o1, Pair o2) {
-                if (o1.dist > o2.dist)
-                    return 1;
-                if (o1.dist < o2.dist)
-                    return 0;
-                if (o1.dist == o2.dist) {
-                    if (o1.x > o2.x)
-                        return 1;
-                    else if (o1.x < o2.y)
-                        return -1;
-                    else if (o1.y < o2.y)
-                        return -1;
-                    else if (o1.y > o2.y)
-                        return 1;
+        br.close();
+
+        int time = 0;
+        int eat = 0;
+        int size = 2;
+        while (true) {
+            PriorityQueue<Pair> fishes = new PriorityQueue<>(new Comparator<Pair>() {
+                @Override
+                public int compare(Pair o1, Pair o2) {
+                    if (o1.dist == o2.dist) {
+                        if (o1.x != o2.x)
+                            return Integer.compare(o1.x, o2.x);
+                        return Integer.compare(o1.y, o2.y);
+                    }
+                    return Integer.compare(o1.dist, o2.dist);
                 }
-                return 0;
-            }
-        });
+            });
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (sea[i][j] >= 1 && sea[i][j] <= 6) {
-                    fishes.offer(new Pair(i, j , bfs(i, j)));
+            boolean[][] visited = new boolean[n][n];
+
+            fishes.offer(new Pair(shark.x, shark.y, 0));
+            visited[shark.x][shark.y] = true;
+
+            boolean check = false;
+            while (!fishes.isEmpty()) {
+                shark = fishes.poll();
+
+                if (sea[shark.x][shark.y] != 0 && sea[shark.x][shark.y] < size) {
+                    sea[shark.x][shark.y] = 0;
+                    eat++;
+                    time+= shark.dist;
+                    check = true;
+                    break;
                 }
-            }
-        }
 
-        while (!fishes.isEmpty()) {
+                for (int i = 0; i < mx.length; i++) {
+                    int tx = shark.x + mx[i];
+                    int ty = shark.y + my[i];
+                    int dist = shark.dist;
 
-        }
-    }
-    static int bfs(int dx, int dy) {
-        boolean[][] visited = new boolean[n][n];
-
-        Pair now = shark;
-        visited[now.x][now.y] = true;
-
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(now);
-
-        int[] mx = {0, 0, -1, 1};
-        int[] my = {-1, 1, 0, 0};
-        while (!queue.isEmpty()) {
-            if (now.x == dx && now.y == dy) {
-                return now.dist;
-            }
-
-            for (int i = 0; i < mx.length; i++) {
-                int tx = now.x + mx[i];
-                int ty = now.y + my[i];
-
-                if ((tx >= 0 && tx < n) && (ty >= 0 && ty < n)) {
-                    if (sea[tx][ty] != 0)
+                    if (tx < 0 || tx >= n || ty < 0 || ty >= n)
                         continue;
-                    if (!visited[tx][ty]) {
+
+                    if (!visited[tx][ty] && sea[tx][ty] <= size){
+                        fishes.offer(new Pair(tx, ty, dist + 1));
                         visited[tx][ty] = true;
-                        queue.offer(new Pair(tx, ty, now.dist + 1));
                     }
                 }
             }
+            if (!check)
+                break;
+            if (size == eat) {
+                size++;
+                eat = 0;
+            }
         }
-        return n * n;
+        System.out.println(time);
     }
     static class Pair {
         public int x;
