318680aaf0dbc9d073a0db4e27d6eb5aef53c944
eycho <chw3872@gmail.com>
2024-01-10 15:40:45 +0900

    Q1806(투포인터)

---
+++ src/Q1806.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1806 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] inputs = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            inputs[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int str = 0;
+        int end = 0;
+        int total = 0;
+        int answer = n + 1;
+        while (str < n && end < n) {
+            if (total < s) {
+                total += inputs[end++];
+            }
+            else if (total >= s) {
+                answer = Math.min(answer, end - str);
+                total -= inputs[str++];
+            }
+        }
+        System.out.println(answer == n + 1 ? 0 : answer);
+    }
+}
