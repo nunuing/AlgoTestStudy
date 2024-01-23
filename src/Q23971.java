8b77a75fb243e94cce4a605a819634cfc615ba23
eycho <chw3872@gmail.com>
2024-01-23 16:09:26 +0900

    Q23971(그리디)

---
+++ src/Q23971.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q23971 {
+    static int h, w, n, m, answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        h = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int horizon = ((w - 1) / (m + 1)) + 1;
+        int vertical =((h - 1)/ (n + 1)) + 1;
+        System.out.println(horizon * vertical);
+    }
+}
