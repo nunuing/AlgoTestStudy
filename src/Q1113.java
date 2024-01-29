685c11a34d7cf45f9b5ec637a63934a1c26f69fa
eycho <chw3872@gmail.com>
2024-01-29 20:11:18 +0900

    Q1113(dfs)

--- src/Q1113.java
+++ src/Q1113.java
@@ -1,22 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Q1113 {
+    static int[][] map;
+    static int n, m, min;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static List<int[]> nodes;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] map = new int[n][m];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n + 2][m + 2];
 
-        for (int i = 0; i < n; i++){
+        for (int i = 1; i <= n; i++){
             char[] temp = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = temp[j] - '0';
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = temp[j - 1] - '0';
             }
         }
-        System.out.println();
+
+        visited = new boolean[n + 2][m + 2];
+        int result = 0;
+        for (int num = 1; num < 10; num++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= m; j++) {
+                    if (map[i][j] == num && !visited[i][j]) {
+                        nodes = new LinkedList<>();
+                        min = 9;
+                        visited[i][j] = true;
+                        dfs(i, j, num);
+                        if (min > num) {
+                            int temp = min - num;
+                            for (int[] pos : nodes) {
+                                map[pos[0]][pos[1]] = min;
+                                visited[pos[0]][pos[1]] = false;
+                                result += temp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+    static void dfs(int x, int y, int num) {
+        nodes.add(new int[]{x, y});
+        for (int i = 0; i < dx.length; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (map[tx][ty] != 0) {
+                if (map[tx][ty] != num) {
+                    min = Math.min(min, map[tx][ty]);
+                    continue;
+                }
+                if (visited[tx][ty])
+                    continue;
+                visited[tx][ty] = true;
+                dfs(tx, ty, num);
+            }
+            else {
+                min = 0;
+            }
+        }
     }
 }
