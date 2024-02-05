7f430e6422c79067b707708ca2e010eb95ba00e8
eycho <chw3872@gmail.com>
2024-02-05 23:47:54 +0900

    Q2638(bfs) solving

--- src/Q2638.java
+++ src/Q2638.java
@@ -2,11 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q2638 {
     static int n, m;
-    static boolean[][] cheese;
+    static boolean[][] cheese, air;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
@@ -24,51 +26,60 @@ public class Q2638 {
             }
         }
 
+        air = new boolean[n][m];
+
         int time = 1;
         while (true) {
+            check();
             ArrayList<int[]> list = new ArrayList<>();
-            boolean flag = false;
-            for (int i = 1; i < n; i++) {
-                for (int j = 1; j < m; j++){
+            for (int i = 1; i < n - 1; i++) {
+                for (int j = 1; j < m - 1; j++) {
                     if (cheese[i][j]) {
-                        flag = true;
-                        if (check(i, j)) {
+                        int cnt = 0;
+                        for (int k = 0; k < dx.length; k++) {
+                            int tx = i + dx[k];
+                            int ty = j + dy[k];
+
+                            if (tx < 0 || tx >= n || ty < 0 || ty >= m)
+                                continue;
+
+                            if (!cheese[tx][ty] && air[tx][ty]) {
+                                cnt++;
+                            }
+                        }
+                        if (cnt >= 2) {
                             list.add(new int[]{i, j});
                         }
                     }
                 }
             }
-
-            if (!flag)
+            if (list.size() == 0)
                 break;
 
             for (int[] melt : list) {
                 cheese[melt[0]][melt[1]] = false;
             }
-            time++;
         }
         System.out.println(time);
     }
-    static boolean check(int x, int y) {
-        int cnt = 0;
-        for (int i = 0; i < dx.length; i++) {
-            int tx = x + dx[i];
-            int ty = y + dy[i];
+    static void check() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        air[0][0] = true;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            for (int i = 0; i < dx.length; i++) {
+                int tx = now[0] + dx[i];
+                int ty = now[1] + dy[i];
 
-            boolean flag = true;
-            while (tx >= 0 && tx < n && ty >= 0 && ty < m){
-                if(cheese[tx][ty]) {
-                    flag = false;
-                    break;
+                if (tx < 0 || tx >= n || ty < 0 || ty >= m)
+                    continue;
+
+                if (!air[tx][ty] && !cheese[tx][ty]) {
+                    air[tx][ty] = true;
+                    queue.offer(new int[]{tx, ty});
                 }
-                tx += dx[i];
-                ty += dy[i];
             }
-            if (flag)
-                cnt++;
         }
-        if (cnt >= 2)
-            return true;
-        return false;
     }
 }
