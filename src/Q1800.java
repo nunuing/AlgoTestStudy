0b2352f920398b56e10f884a1858e67d47bba96a
eycho <chw3872@gmail.com>
2024-02-07 22:04:22 +0900

    Q1800 solving

--- src/Q1800.java
+++ src/Q1800.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q1800 {
     static int n, p, k;
-    static int[][] computers;
+    static ArrayList<Cable>[] computers;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,15 +14,18 @@ public class Q1800 {
         p = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        computers = new int[n + 1][n + 1];
+        computers = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++)
+            computers[i] = new ArrayList<>();
+
         for (int i = 0; i < p; i++) {
             st = new StringTokenizer(br.readLine());
             int c1 = Integer.parseInt(st.nextToken());
             int c2 = Integer.parseInt(st.nextToken());
             int price = Integer.parseInt(st.nextToken());
 
-            computers[c1][c2] = price;
-            computers[c2][c1] = price;
+            computers[c1].add(new Cable(c2, price));
+            computers[c2].add(new Cable(c1, price));
         }
 
         int left = 0;
@@ -44,45 +44,38 @@ public class Q1800 {
         System.out.println(answer);
     }
 
-    static boolean check(int price) {
-        Queue<Computer> queue = new LinkedList<>();
+    static boolean check(int mid) {
+        PriorityQueue<Cable> pq = new PriorityQueue<>();
+        pq.offer(new Cable(1, 0));
         int[] visited = new int[n + 1];
-        Arrays.fill(visited, -1);
 
-        queue.offer(new Computer(1, 1));
+        Arrays.fill(visited, Integer.MAX_VALUE);
         visited[1] = 0;
-        while (!queue.isEmpty()) {
-            Computer now = queue.poll();
-            if (now.num == n) {
-                if (visited[n] == price)
-                    return true;
-                else
-                    return false;
+        while (!pq.isEmpty()){
+            Cable now = pq.poll();
+            int nn = now.num;
+            int price = now.price;
+            if (visited[nn] < mid)
+                continue;
+
+            for (Cable next : computers[nn]) {
+
             }
 
-            int nn = now.num;
-            int nc = now.cables;
-            int np = visited[now.num];
-            for (int i = 1; i <= n; i++) {
-                if (computers[nn][i] != 0 && visited[i] == -1) {
-                    if (nc > k) {
-                        visited[i] = Math.max(visited[i], computers[nn][i]);
-                    }
-                    else {
-                        visited[i] = 0;
-                    }
-                    queue.offer(new Computer(i, nc + 1));
-                }
-            }
         }
-        return false;
+        return visited[n] <= k;
     }
-    static class Computer {
+    static class Cable implements Comparable<Cable> {
         public int num;
-        public int cables;
-        public Computer(int num, int cables) {
+        public int price;
+        public Cable(int num, int price) {
             this.num = num;
-            this.cables = cables;
+            this.price = price;
+        }
+
+        @Override
+        public int compareTo(Cable o) {
+            return this.price - o.price;
         }
     }
 }
