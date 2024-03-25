32ed58dd4a1805ff074e5891579833b8d422366a
eycho <chw3872@gmail.com>
2024-03-25 12:49:57 +0900

    P15736(수학)

---
+++ src/P15736.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P15736 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int num = 1;
+        while (num * num <= n) {
+            answer++;
+            num++;
+        }
+        System.out.println(answer);
+    }
+}
