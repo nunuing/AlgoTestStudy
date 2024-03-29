8b3efe74f7c37679cef13c85e1ea80f561cf1db3
eycho <chw3872@gmail.com>
2024-03-29 14:17:19 +0900

    Q10815_2(hashset)

--- src/Q10815_3.java
+++ src/Q10815_3.java
@@ -1,42 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Q10815_2 {
+public class Q10815_3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> hs = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] cards = new int[n];
         for (int i = 0; i < n; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+            hs.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(cards);
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             int target = Integer.parseInt(st.nextToken());
-            int left = 0;
-            int right = n - 1;
-            boolean check = false;
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (cards[mid] == target) {
-                    check = true;
-                    break;
-                }
-                else if(cards[mid] < target) {
-                    left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
-            }
-            sb.append((check ? '1' : '0') + " ");
+            sb.append((hs.contains(target) ? 1 : 0) + " ");
         }
         System.out.println(sb);
     }
